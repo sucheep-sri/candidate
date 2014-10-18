1e13e7d4fc2e018a8db375d942a3daeba6677e18
unknown <sucheep.s@taskworld.com>
2014-10-18 17:16:42 +0700

    motest1

--- test1.js
+++ test1.js
@@ -9,10 +9,10 @@ In put the 3 of 'X' by index at x1, x2, x3 and input stating and ending point by
 ////////////////
 */
 var x1 = [2,3]; // !!! change first X here !!!
-var x2 = [4,3]; // !!! change second X here !!!
-var x3 = [5,3]; // !!! change third X  here !!!
-var start = [4,4];  // !!! change starting point here !!!
-var end = [4,2];  // !!! change ending point here !!!
+var x2 = [3,3]; // !!! change second X here !!!
+var x3 = [1,4]; // !!! change third X  here !!!
+var start = [2,4];  // !!! change starting point here !!!
+var end = [1,3];  // !!! change ending point here !!!
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 var indexMetrix = [];
@@ -23,74 +23,83 @@ var sTemp = [];
 var step = 0;
 var endFound = false;
 var xFound = true;
+var answerArr = [];
 var answer = 0;
 
 createIndexMetrix();
 createInputMetrix();
 traverse();
+findShortest();
 console.log("Here S is the starting point and E is the Ending point. Shortest path is " + answer);
 
 
 function traverse()
 {
-	for(var i =parseInt(start[0]); i>0; i--) // go left horizon and up vertical
+	for(var i =parseInt(start[0]); i>0; i--) // go up
 	{
-		for(var j =parseInt(start[1]); j>0; j--)
+		for(var j =parseInt(start[1]); j>0; j--) //go left
 		{
 			tempIndex = [i,j];
 			console.log("-------------------------------check row at : " + i + "-------------------------------");
 			checkX(tempIndex);
 			checkEndPoint(tempIndex, step);
-			//checkRowBlocker(i);
 			console.log("At : " + tempIndex + " step : " + step );
-			if(endFound == true && xFound == false)
+			if(xFound)
 			{
-				console.log("************ Found Shortest !" + answer);
+				console.log("BREAK THIS ROW !" + i);
+				break;
 			}
 			else
 			{
-				if(j<start[1])// check vertical
+				if(endFound == true && xFound == false)
 				{
-					checkInsideVertical(step, i, j);
-				}	
+					console.log("************ Found Shortest !" + answer);
+				}
+					if(j<start[1])// check vertical
+					{
+						checkInsideVertical(step, i, j);
+					}	
+				step++;
 			}
-			step++;
 		}
 		step = 0;
 		step++;
 	}
-	if(tempIndex.toString() != end.toString()) // go right horizon and up vertical
+	if(tempIndex.toString() != end.toString()) 
 	{
 		step = 0;
-		console.log("GO RIGHT ! ");
-		for(var i =parseInt(start[0]); i<=5; i++) 
+		console.log("GO down ! ");
+		for(var i =parseInt(start[0]); i<=5; i++) // go down
 		{
-			for(var j =parseInt(start[1]); j<=5; j++)
+			for(var j =parseInt(start[1]); j<=5; j++) // go right
 			{
 				tempIndex = [i,j];
 				console.log("-------------------------------check row at : " + i + "-------------------------------");
 				checkX(tempIndex);
 				checkEndPoint(tempIndex, step);
-				//checkRowBlocker(i);
 				console.log("At : " + tempIndex + " step : " + step );
-				if(endFound == true && xFound == false)
+				if(xFound)
 				{
-					console.log("************ Found Shortest !" + answer);
+					console.log("BREAK THIS ROW !" + i);
+					break;
 				}
 				else
 				{
-					if(j>start[1])
+					if(endFound == true && xFound == false)
 					{
-						checkInsideVertical(step, i, j);
-					}	
-				}	
-				step++;
+						console.log("************ Found Shortest !" + answer);
+					}
+						if(j>start[1])
+						{
+							checkInsideVertical(step, i, j);
+						}	
+					step++;
+				}
 			}
 			step = 0;
 			step++;
 		}
 	}		
-//console.log("S to E is : " + step);
 }
 function checkInsideVertical(step, i, j)
 {
@@ -102,17 +111,69 @@ function checkInsideVertical(step, i, j)
 		checkX(vTemp);
 		checkEndPoint(vTemp, sTemp);
 		console.log("At : " + vTemp + " step : " + sTemp );
+		if(xFound)
+		{
+			onsole.log("BREAK THIS Column !" + k);
+			break;
+		}
+		else
+		{
+			if(endFound == true && xFound == false)
+			{
+				console.log("************ Found Shortest !" + answer);
+			}
+			if(k < i)
+			{
+				checkInsideHorizontal(step, k, j)
+			}	
+		}
+	}
+	sTemp = step;
+	for(var k = i+1; k<=5; k++) // check vertical down
+	{
+		sTemp++;
+		vTemp = [k,j];
+		checkX(vTemp);
+		checkEndPoint(vTemp, sTemp);
+		console.log("At : " + vTemp + " step : " + sTemp );
+		if(xFound)
+		{
+			onsole.log("BREAK THIS Column !" + k);
+			break;
+		}
+		else
+		{
+			if(endFound == true && xFound == false)
+			{
+				console.log("************ Found Shortest ! " + answer);
+			}
+			if(k > i)
+			{
+				checkInsideHorizontal(step, k, j)
+			}
+		}
+	}
+}
+function checkInsideHorizontal(step, k, j)
+{
+	sTemp = step;
+	for(var l = j-1; l>0; l--) // check vertical up
+	{
+		sTemp++;
+		vTemp = [k,l];
+		checkX(vTemp);
+		checkEndPoint(vTemp, sTemp);
+		console.log("At : " + vTemp + " step : " + sTemp );
 		if(endFound == true && xFound == false)
 		{
 			console.log("************ Found Shortest !" + answer);
 		}
-		//checkEndPoint(vTemp, sTemp);
 	}
 	sTemp = step;
-	for(var l = i+1; l<=5; l++) // check vertical down
+	for(var l = j+1; l<=5; l++) // check vertical down
 	{
 		sTemp++;
-		vTemp = [l,j];
+		vTemp = [k,l];
 		checkX(vTemp);
 		checkEndPoint(vTemp, sTemp);
 		console.log("At : " + vTemp + " step : " + sTemp );
@@ -120,7 +181,6 @@ function checkInsideVertical(step, i, j)
 		{
 			console.log("************ Found Shortest ! " + answer);
 		}
-		//checkEndPoint(vTemp, sTemp);
 	}
 }
 
@@ -130,41 +190,29 @@ function checkX(tempIndex)
 	{
 		console.log("///////////////////////////////////////////Found X1 AT : "+tempIndex); 
 		xFound = true;
-		//return false;
 	}
 	if(tempIndex.toString() == x2.toString())
 	{
 		console.log("///////////////////////////////////////////Found X2 AT : "+tempIndex); 
 		xFound = true;
-		//return false;
 	}
 	if(tempIndex.toString() == x3.toString())
 	{
 		console.log("///////////////////////////////////////////Found X3 AT : "+tempIndex); 
 		xFound = true;
-		//return false;
 	}
 	if(tempIndex.toString() != x1.toString() && tempIndex.toString() != x2.toString() && tempIndex.toString() != x3.toString())
 	{
 		xFound = false;
-		//return true;
 	}
 }
 
-/*function checkRowBlocker(i)
-{
-	if(blocker == false)
-	{
-		rowBlocker.push(false);
-		console.log("++++++++++++++++++++++++++++++++++++++++++++++++++++++++Row blocker at row : " + rowBlocker);
-	}
-}*/
 function checkEndPoint(tempIndex, step)
 {
 	if(tempIndex.toString() == end.toString()) //if end found
 	{
 		console.log("*******************************************Found end point ! Step : " + step + " END POINT AT : " +tempIndex);
-		answer = step;
+		answerArr.push([step]);
 		endFound = true;
 	}
 	else
@@ -172,7 +220,10 @@ function checkEndPoint(tempIndex, step)
 		endFound = false;
 	}
 }
-
+function findShortest()
+{
+	answer = Math.min.apply(Math, answerArr);
+}
 function createIndexMetrix()
 {
 	for(var i =0; i<=4; i++) 
